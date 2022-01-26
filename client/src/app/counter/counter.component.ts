1715ed9782156fe7efbfe7d73531a3b3a333a7aa
praveensinghkmr <praveensingh.kmr@gmail.com>
2022-01-27 01:48:37 +0530

    coutner component

---
+++ client/src/app/counter/counter.component.ts
@@ -0,0 +1,15 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "counter",
+    template:` Test counter`,
+    styles:[],
+})
+
+export class CounterComponent{
+
+    // How to use the counter class here ?
+    
+}
+
+
