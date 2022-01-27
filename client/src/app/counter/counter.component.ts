97a13e9516214b508c83e730005479f0823a7e5c
praveensinghkmr <praveensingh.kmr@gmail.com>
2022-01-27 22:27:34 +0530

    counter component

--- client/src/app/counter/counter.component.ts
+++ client/src/app/counter/counter.component.ts
@@ -2,14 +2,34 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: "counter",
-    template:` Test counter`,
-    styles:[],
+    template: `    
+    <div>
+    <button (click)="increment()">+</button>
+        <div>{{getCount()}}</div>
+        <button (click)="decrement()">-</button>
+    </div>
+`,
+    styles: [],
 })
 
-export class CounterComponent{
+export class CounterComponent {
 
-    // How to use the counter class here ?
-    
+    private count: number;
+    constructor() {
+        this.count = 0;
+    }
+
+    increment() {
+        this.count++;
+    }
+
+    decrement() {
+        this.count--;
+    }
+
+    getCount() {
+        return this.count;
+    }
 }
 
 
